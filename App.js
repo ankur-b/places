fca13d3fe5221a3f5616985359823fd70e8d2590
ankur-b <barveankur29@outlook.com>
2021-06-07 16:36:39 +0530

    Screen and Navigation setup done

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlacesNaviator from './src/navigation/PlacesNavigator'
+const App= () => {
+  return(
+    <PlacesNaviator/>
+  )
+};
+
+
+export default App;
---
+++ src/navigation/PlacesNavigator.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Platform} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import PlacesListScreen from '../screens/PlacesListScreen';
+import PlaceDetailScreen from '../screens/PlaceDetailScreen';
+import NewPlaceScreen from '../screens/NewPlaceScreen';
+import MapScreen from '../screens/MapScreen';
+import Colors from '../constants/Colors';
+import {HeaderButtons, Item} from 'react-navigation-header-buttons';
+import CustomHeaderButton from '../components/HeaderButton';
+const Stack = createStackNavigator();
+const PlacesNavigator = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primary : '',
+          },
+          headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primary,
+        }}>
+        <Stack.Screen
+          name="Places"
+          component={PlacesListScreen}
+          options={({navigation}) => ({
+            title: 'All Places',
+            headerRight: props => (
+              <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+                <Item
+                  title="Add Places"
+                  iconName={Platform.OS === 'android' ? 'md-add' : 'ios-add'}
+                  onPress={() => {
+                    navigation.navigate("NewPlace")
+                  }}
+                />
+              </HeaderButtons>
+            ),
+          })}
+        />
+        <Stack.Screen name="PlacesDetail" component={PlaceDetailScreen} />
+        <Stack.Screen name="NewPlace" component={NewPlaceScreen} options={()=>({
+          title:"Add Place"
+        })}/>
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default PlacesNavigator;
